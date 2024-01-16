297dc9cdf711663bb53b21d1e13da9e2faa82836
Darnell Baird <drsbaird@yahoo.com>
2024-01-16 09:45:15 -0400

    Update: Main: Removed unneeded code.

--- main.py
+++ main.py
@@ -3,16 +3,29 @@
 import logging
 import os
 import configparser
+import json
 
 import pandas as pd
 import openpyxl
 
+# Logging
 logging.basicConfig(
     # filename='example.log', 
     encoding='utf-8', level=logging.INFO
     )
 
 logging.info("Copying excel sheets from multiple files to one file")
+
+# CONFIGURATION
+config = dict()
+with open("config.json", "r") as jsonfile:
+    config = json.load(jsonfile)
+    logging.info("Read 'config.json' successful.")
+
+# CONFIGURATIONS
+quiet = config['DEFAULT'].get('quiet', False)
+excel_files_dir = config['DEFAULT'].get('excel_files_dir', os.getcwd())
+output_excel_file_name = config['DEFAULT'].get('output_excel_file_name', 'Combined/combined_file.xlsx')
 
 # FUNCTIONS
 def ensure_str_unique(input_str:str, str_list:list[str]=[]):
@@ -33,79 +46,51 @@ def ensure_str_unique(input_str:str, str_list:list[str]=[]):
         raise OverflowError("Code Error: Str options exceeded.")
     return input_str
 
-# Setup Configuration reader
-config = configparser.ConfigParser()
-config.read('config.ini')
+def is_excel_file(file_name):
+    return any((file_name.endswith(ext) for ext in ('.xls','.xlsx')))
 
-# Configuration inputs
-quiet = config['DEFAULT'].get('quiet', False)
-if isinstance(quiet, str):
-    quiet = 'true'==quiet
-cwd = os.getcwd(); # os.path.abspath('')
-excel_files_dir = config['DEFAULT'].get('excel_files_dir', cwd)
-output_excel_file_name = config['DEFAULT'].get('output_excel_file_name', 'Combined/combined_file.xlsx')
-
-# Panda files setup
-df_total = pd.DataFrame()
-df_total.to_excel(output_excel_file_name) #create a new file
-workbook=openpyxl.load_workbook(output_excel_file_name)
-ss_sheet = workbook['Sheet1']
-ss_sheet.title = 'TempExcelSheetForDeleting'
-workbook.save(output_excel_file_name)
-
-files = os.listdir(excel_files_dir)
+_df_rename_rename_redundants_dict = dict()
+def _df_rename_rename_redundants():
+    """ 
+    There is a method in panda called 'df.rename'.
+    It requires that the data be structured as {old_field_name:new_field_name}
+    for each field to rename.
+    The configuration variable "Combine_Fields" has a different structure and 
+    must be restructured for the .rename method.
+      """
+    global _df_rename_rename_redundants_dict
+    for k,v_list in config["Combine_Fields"].items():
+        for val in v_list:
+            _df_rename_rename_redundants_dict.update({val:k})
+_df_rename_rename_redundants()
+assert len(_df_rename_rename_redundants_dict) > 0, "This should not be empty."
 
 # Check-out each excel file and then combine
-""" 
-for file in files:
-    try:
-        # Is it an excel file?
-        if any((file.endswith(ext) for ext in ('.xls','.xlsx'))):
-            file_path = os.path.join(excel_files_dir, file)
-            excel_file = pd.ExcelFile(file_path)
-            sheets = excel_file.sheet_names
-            # loop through sheets inside an Excel file
-            for sheet in sheets:
-                str_sheet = str(sheet)
-                logging.info(f"{file}, {sheet}")
-                df = excel_file.parse(sheet_name=sheet)
-                with pd.ExcelWriter(output_excel_file_name,mode='a') as writer:  
-                    sheet_name = compare_str_list(str_sheet)
-                    if sheet_name != str_sheet:
-                        logging.info(f"The sheet named '{str_sheet}' changed to '{sheet_name}'.")
-                    df.to_excel(writer, sheet_name=str(sheet_name), index=False)
-                #df.to_excel(output_excel_file_name, sheet_name=f"{sheet}")
-    except PermissionError:
-        logging.warning(f"The file '{file}' cannot be opened.") 
-"""
 frames = []
-with pd.ExcelWriter(output_excel_file_name,mode='a') as writer:
+files = os.listdir(excel_files_dir)
+with pd.ExcelWriter(output_excel_file_name,mode='w') as writer:
     for file in files:
         try:
             # Is it an excel file?
-            if any((file.endswith(ext) for ext in ('.xls','.xlsx'))):
+            if is_excel_file(file):
                 file_path = os.path.join(excel_files_dir, file)
                 excel_file = pd.ExcelFile(file_path)
                 sheets = excel_file.sheet_names
                 # loop through sheets inside an Excel file
                 for sheet in sheets:
-                    str_sheet = str(sheet)
-                    logging.info(f"{file}, {sheet}")
+                    logging.info(f"File: {file} | Sheet: {sheet}")
                     df = excel_file.parse(sheet_name=sheet)
+                    # Ignore empty dataframes
+                    if df.empty: continue
+                    # Rename redundants Excel Fields
+                    df.rename(columns=_df_rename_rename_redundants_dict)
+                    # Add to be concatenated
                     frames.append(df)
         except PermissionError:
-            logging.warning(f"The file '{file}' cannot be opened.") 
+            logging.warning(f"The file '{file}' cannot be opened.")
+    # Create one sheet containing all spreadsheet data.
     result = pd.concat(frames)
-    # df.to_excel(writer, sheet_name='sheet1', index=False)
-    result.to_excel(writer, sheet_name='sheet1', index=False)
+    result.to_excel(writer, index=False)
 
-
-workbook=openpyxl.load_workbook(output_excel_file_name)
-std=workbook["TempExcelSheetForDeleting"]
-workbook.remove(std)
-workbook.save(output_excel_file_name)
-logging.info("Loaded, press ENTER to end")
-if not quiet: dali=input()
-#df_total.to_excel(output_excel_file_name)
-logging.info("Done")
+logging.info("Reading files complete. Done.")
 if not quiet: dali=input()
