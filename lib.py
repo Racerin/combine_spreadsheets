889fa0c4df30123965d3f2456b1859d99b48801e
Darnell Baird <drsbaird@yahoo.com>
2024-01-17 14:12:37 -0400

    feat: Unique columns functions
    Migrate some functions to lib.py. Create functions for unique column. Encapulate main(). Added fields to json template

---
+++ lib.py
@@ -0,0 +1,61 @@
+import re
+
+# FUNCTIONS
+def ensure_str_unique(input_str:str, str_list:list[str]=[]):
+    """ 
+    If 'input_str' is inside of list, update it's name and try again to add it.
+    Return the accepted str.
+    'str_list' automatically updates with each tested 'input_str'
+    """
+    n_str_list = len(str_list)
+    for _ in range(n_str_list + 1):
+        if input_str in str_list:
+            # Append to 'input_str' name and try again
+            input_str = input_str + "X"
+        else:
+            str_list.append(input_str)
+            break
+    else:
+        raise OverflowError("Code Error: Str options exceeded.")
+    return input_str
+
+def is_excel_file(file_name):
+    return any((file_name.endswith(ext) for ext in ('.xls','.xlsx')))
+
+def reduce_whitespace_to_one_space(str1:str):
+    # return re.sub(r'\s+'," ", str1).strip()
+    return str1
+
+def dict__old_to_unique_new_field(
+        columns:list[str], 
+        strip:bool=False,
+        titleize:bool=False,
+        lower:bool=False,
+        upper:bool=False,
+        )->dict(str=str):
+    # reduce whitespace to one space
+    new_strs = [re.sub(r'\s+'," ",old_str) for old_str in columns]
+    # Apply to strings according to flag
+    if strip: new_strs = [str1.strip() for str1 in new_strs]
+    if titleize: new_strs = [str1.title() for str1 in new_strs]
+    if lower: new_strs = [str1.lower() for str1 in new_strs]
+    if upper: new_strs = [str1.upper() for str1 in new_strs]
+    assert all((not s.endswith(" ") and not s.startswith(" ") for s in new_strs))
+    # Make each unique str
+    uniques = list()
+    for str1 in new_strs:
+        n_uniques = len(uniques)
+        for _ in range(n_uniques+1):
+            if str1 in uniques:
+                # Rename str1 and try again
+                str1 += "X"
+            else:
+                # Go ahead and add the str1
+                break
+        else:
+            raise OverflowError(f"'{str1}' should be found in list '{new_strs}'.")
+        uniques.append(str1)
+    assert len(uniques) == len(columns), str(uniques)
+    # convert to dict with old_str:new_str format
+    ret_dict = {old:new for (old,new) in zip(columns,uniques)}
+    return ret_dict
--- main.py
+++ main.py
@@ -2,11 +2,12 @@
 
 import logging
 import os
-import configparser
 import json
 
 import pandas as pd
-import openpyxl
+# import openpyxl
+
+from lib import *
 
 # Logging
 logging.basicConfig(
@@ -26,71 +27,72 @@ with open("config.json", "r") as jsonfile:
 quiet = config['DEFAULT'].get('quiet', False)
 excel_files_dir = config['DEFAULT'].get('excel_files_dir', os.getcwd())
 output_excel_file_name = config['DEFAULT'].get('output_excel_file_name', 'Combined/combined_file.xlsx')
+rename_columns = config['DEFAULT'].get('rename_columns', False)
+reduce_field_whitespace : bool = config['DEFAULT'].get('reduce_field_whitespace', False)
+assert all((isinstance(var, bool) for var in [quiet, rename_columns, reduce_field_whitespace,])), "One of the flags are not a bool"
 
 # FUNCTIONS
-def ensure_str_unique(input_str:str, str_list:list[str]=[]):
-    """ 
-    If 'input_str' is inside of list, update it's name and try again to add it.
-    Return the accepted str.
-    'str_list' automatically updates with each tested 'input_str'
-    """
-    n_str_list = len(str_list)
-    for _ in range(n_str_list + 1):
-        if input_str in str_list:
-            # Append to 'input_str' name and try again
-            input_str = input_str + "X"
-        else:
-            str_list.append(input_str)
-            break
-    else:
-        raise OverflowError("Code Error: Str options exceeded.")
-    return input_str
 
-def is_excel_file(file_name):
-    return any((file_name.endswith(ext) for ext in ('.xls','.xlsx')))
 
-_df_rename_rename_redundants_dict = dict()
-def _df_rename_rename_redundants():
+_df_rename__rename_redundants_dict = dict()
+def _df_rename__rename_redundants():
     """ 
     There is a method in panda called 'df.rename'.
     It requires that the data be structured as {old_field_name:new_field_name}
     for each field to rename.
     The configuration variable "Combine_Fields" has a different structure and 
     must be restructured for the .rename method.
+    Ref: https://stackoverflow.com/a/11354850
       """
-    global _df_rename_rename_redundants_dict
+    global _df_rename__rename_redundants_dict
     for k,v_list in config["Combine_Fields"].items():
         for val in v_list:
-            _df_rename_rename_redundants_dict.update({val:k})
-_df_rename_rename_redundants()
-assert len(_df_rename_rename_redundants_dict) > 0, "This should not be empty."
+            _df_rename__rename_redundants_dict.update({val:k})
+_df_rename__rename_redundants()
+# assert len(_df_rename__rename_redundants_dict) > 0, "This should not be empty."
+logging.debug(_df_rename__rename_redundants_dict)
 
-# Check-out each excel file and then combine
-frames = []
-files = os.listdir(excel_files_dir)
-with pd.ExcelWriter(output_excel_file_name,mode='w') as writer:
-    for file in files:
-        try:
-            # Is it an excel file?
-            if is_excel_file(file):
-                file_path = os.path.join(excel_files_dir, file)
-                excel_file = pd.ExcelFile(file_path)
-                sheets = excel_file.sheet_names
-                # loop through sheets inside an Excel file
-                for sheet in sheets:
-                    logging.info(f"File: {file} | Sheet: {sheet}")
-                    df = excel_file.parse(sheet_name=sheet)
-                    # Ignore empty dataframes
-                    if df.empty: continue
-                    # Rename redundants Excel Fields
-                    df.rename(columns=_df_rename_rename_redundants_dict)
-                    # Add to be concatenated
-                    frames.append(df)
-        except PermissionError:
-            logging.warning(f"The file '{file}' cannot be opened.")
-    # Create one sheet containing all spreadsheet data.
-    result = pd.concat(frames)
-    result.to_excel(writer, index=False)
+def reduce_field_whitespace_custom(str1):
+    """ Wrapper for 'dict__old_to_unique_new_field' """
+    return dict__old_to_unique_new_field(str1, strip=True, titleize=True)
 
-logging.info("Reading files complete. Done.")
-if not quiet: dali=input()
+def main():
+    # Check-out each excel file and then combine
+    frames = []
+    files = os.listdir(excel_files_dir)
+    with pd.ExcelWriter(output_excel_file_name,mode='w') as writer:
+        for file in files:
+            try:
+                # Is it an excel file?
+                if is_excel_file(file):
+                    file_path = os.path.join(excel_files_dir, file)
+                    excel_file = pd.ExcelFile(file_path)
+                    sheets = excel_file.sheet_names
+                    # loop through sheets inside an Excel file
+                    for sheet in sheets:
+                        logging.info(f"File: {file} | Sheet: {sheet}")
+                        df = excel_file.parse(sheet_name=sheet)
+                        # Ignore empty dataframes
+                        if df.empty: continue
+                        # Rename redundants Excel Fields
+                        # Remove whitespace in all columns
+                        if reduce_field_whitespace:
+                            # df.rename(reduce_whitespace_to_one_space, axis="columns", inplace=True)
+                            columns = df.columns.to_list()
+                            new_column_names_dict = reduce_field_whitespace_custom(columns)
+                            df.rename(columns=new_column_names_dict, inplace=True)
+                        if rename_columns:
+                            df.rename(columns=_df_rename__rename_redundants_dict, inplace=True)
+                        # Add to be concatenated
+                        frames.append(df)
+            except PermissionError:
+                logging.warning(f"The file '{file}' cannot be opened.")
+        # Create one sheet containing all spreadsheet data.
+        result = pd.concat(frames)
+        result.to_excel(writer, index=False)
+
+    logging.info("Reading files complete. Press ENTER to continue.")
+    if not quiet: dali=input()
+
+if __name__ == '__main__':
+    main()
